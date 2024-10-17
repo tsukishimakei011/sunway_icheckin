31fe8be3e77ddc27c5ed615638c1c535835000ac
Rachel Lam <119136244+tsukishimakei011@users.noreply.github.com>
2024-10-17 08:11:13 +0800

    Delete delete_credentials.py

--- delete_credentials.py
+++
@@ -1,8 +0,0 @@
-# Function to clear the contents of the credentials file
-def clear_credentials():
-    with open('credentials.txt', 'w') as file:
-        file.write("")  # Overwrite with an empty string
-    print("Credentials cleared.")
-
-# Call the function to clear the file
-clear_credentials()
